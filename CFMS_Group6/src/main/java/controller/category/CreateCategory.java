e79a501e0886ac774102af576a840834c51993bf
QPTA <donhatquang401@gmail.com>
2026-02-12 19:58:24 +0700

    WIP: show status in CRUD

--- CFMS_Group6/src/main/java/controller/category/CreateCategory.java
+++ CFMS_Group6/src/main/java/controller/category/CreateCategory.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package controller.category;
 
 import dal.CategoryDao;
@@ -26,61 +23,49 @@ public class CreateCategoryController extends HttpServlet {
         request.getRequestDispatcher("/views/category/category-form.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    CategoryDao cDao = new CategoryDao();
+    
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String status = "";
+        String status;
+        CategoryDao cDao = new CategoryDao();
         try {
             String category_name = request.getParameter("category_name");
             if(category_name.isBlank()){
                 status = "Tên danh mục không được để trống";
                 request.setAttribute("status", status);
-                response.sendRedirect("CreateCategoryController");
+                request.setAttribute("categoryForm", "create");
+                request.getRequestDispatcher("/views/category/category-form.jsp").forward(request, response);
                 return;
             }
             String prefix_code = request.getParameter("prefix_code");
             if(prefix_code.isBlank()){
                 status = "Tiền tố danh mục không được để trống";
                 request.setAttribute("status", status);
-                response.sendRedirect("CreateCategoryController");
+                request.setAttribute("categoryForm", "create");
+                request.getRequestDispatcher("/views/category/category-form.jsp").forward(request, response);
                 return;
             }
             String description = request.getParameter("description");
             if(description.isBlank()){
                 status = "Mô tả danh mục không được để trống";
                 request.setAttribute("status", status);
-                response.sendRedirect("CreateCategoryController");
+                request.setAttribute("categoryForm", "create");
+                request.getRequestDispatcher("/views/category/category-form.jsp").forward(request, response);
                 return;
             }
             cDao.createCategory(category_name, prefix_code, description);
             status = "Tạo danh mục mới thành công";
             request.setAttribute("status", status);
+            request.setAttribute("categoryForm", "create");
+            request.getRequestDispatcher("/views/category/category-form.jsp").forward(request, response);
         } catch (IOException e) {
             status = "Lỗi khi tạo danh mục.";
             request.setAttribute("status", status);
+            request.setAttribute("categoryForm", "create");
+            request.getRequestDispatcher("/views/category/category-form.jsp").forward(request, response);
         }
-        response.sendRedirect("ViewCategoryController");
-
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
