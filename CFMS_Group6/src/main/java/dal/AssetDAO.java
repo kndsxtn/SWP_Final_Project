79a5800369ac5e96dab0f15dc0e8550d10d6ba13
Tung <tung39315@gmail.com>
2026-02-24 19:23:49 +0700

    fix conflict assetDao

--- CFMS_Group6/src/main/java/dal/AssetDAO.java
+++ CFMS_Group6/src/main/java/dal/AssetDAO.java
@@ -537,4 +537,27 @@ public class AssetDAO {
         else
             ps.setNull(index, java.sql.Types.DATE);
     }
+    
+    /*Phần của pvtung*/
+    public List<Asset> getByRoomId(int id) {
+        String sql = "SELECT * FROM assets WHERE room_id = ?";
+        try (Connection con = new DBContext().getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            List<Asset> assets = new ArrayList<>();
+            while (rs.next()) {
+                Asset a = new Asset();
+                a.setAssetId(rs.getInt("asset_id"));
+                a.setAssetCode(rs.getString("asset_code"));
+                a.setAssetName(rs.getString("asset_name"));
+                a.setStatus(rs.getString("status"));
+                assets.add(a);
+            }
+            return assets;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
