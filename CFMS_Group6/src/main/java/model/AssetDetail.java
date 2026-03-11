b7e07055d2ca8edbb20a0f8ba2a0e3bd096a6069
Tung <ting39315@gmail.com>
2026-03-11 13:31:47 +0700

    fix transfer process after update data base

---
+++ CFMS_Group6/src/main/java/model/AssetDetail.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class AssetDetail {
+    private int instanceId;
+    private int assetId;
+    private String instanceCode;
+    private int roomId;
+    private String status;
+
+    public AssetDetail() {
+    }
+
+    public AssetDetail(int instanceId, int assetId, String instanceCode, int roomId, String status) {
+        this.instanceId = instanceId;
+        this.assetId = assetId;
+        this.instanceCode = instanceCode;
+        this.roomId = roomId;
+        this.status = status;
+    }
+
+    public int getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(int instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public int getAssetId() {
+        return assetId;
+    }
+
+    public void setAssetId(int assetId) {
+        this.assetId = assetId;
+    }
+
+    public String getInstanceCode() {
+        return instanceCode;
+    }
+
+    public void setInstanceCode(String instanceCode) {
+        this.instanceCode = instanceCode;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
